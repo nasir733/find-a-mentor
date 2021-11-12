2c0a49722997050933ab1f5b1a625c74bb9d0b25
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-11-12 07:02:03 +0530

    major update 12

--- dashboard/views.py
+++ dashboard/views.py
@@ -157,7 +157,7 @@ def joinmeeting(request):
 @login_required(login_url='/dashboard/login/')
 def mentorcontent(request):
     context = {}
-    mentor_content = Content.objects.filter(mentor=request.user.mentorprofile, approved=True)
+    mentor_content = MentorRequest.objects.filter(mentee=request.user.menteeprofile)
     context['mentor_content'] = mentor_content
     return render(request, 'dashboard/mentorcontent.html', context=context)
 
