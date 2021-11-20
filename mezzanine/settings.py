4001cdbf1d0a851b0487821867d0e86ae857182d
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-11-20 09:38:06 +0530

    nov 20 update

--- mezzanine/settings.py
+++ mezzanine/settings.py
@@ -120,8 +120,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=600, ssl_require=True)
 
 
 # Password validation
