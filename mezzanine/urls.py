747356f61355db8b083b5075dbbe4607d9752371
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-11-28 00:56:47 +0530

    forget password

--- mezzanine/urls.py
+++ mezzanine/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'),
      name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset/password_resetform.html'), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset/password_reset_completeemail.html'), name='password_reset'),
     
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
      name='password_reset_complete'),
