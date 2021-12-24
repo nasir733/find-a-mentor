63f991792f7a9f6de227fd48e19103c653992503
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-12-24 22:58:55 +0500

    Wrote a custom script to add

--- mezzanine/settings.py
+++ mezzanine/settings.py
@@ -5,6 +5,7 @@ import dotenv
 from pathlib import Path
 import django_heroku
 import os
+import sys
 dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
